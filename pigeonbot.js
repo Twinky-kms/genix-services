af740b0b2bb4b130b1572206c6fc9ffb1b9d54c4
Luke Pighetti <lukepighetti@gmail.com>
2018-04-27 10:50:38 -0400

    refactor

--- pigeonbot.js
+++ pigeonbot.js
@@ -6,6 +6,8 @@ const client = new Discord.Client();
 const https = require('https');
 var data = {};
 
+var stringSimilarity = require('string-similarity');
+
 
 
 main();
@@ -17,14 +19,16 @@ function main(){
     .then(json => {
     data = json;
 
-/*    data.price = json.price
-    data.volume = 
-    data.marketCap = 
-    data.supply = 
-    data.hashrate = 
-    data.difficulty = 
-    data.blockTime = 
-    data.retarget = */
+    /*
+    data.price
+    data.volume
+    data.marketCap
+    data.supply
+    data.hashrate
+    data.difficulty
+    data.blockTime
+    data.retarget
+    */
   })
 }
 
@@ -57,7 +61,129 @@ function promiseJSON(url) {
 }
 
 
+//////////////////////
+// stripPrefix function
+//////////////////////
 
+function cleanMessage(message){
+  let deleteThis = ['!','when','help','who','what','where','how','why']
+  let result = message
+
+  for(item in deleteThis){
+    result = result.replace(deleteThis[item],'');
+  }
+
+  // delete all leading and trailing spaces
+  result = result.trim()
+
+  return result;
+}
+
+
+
+function getReply(cleanedMessage){
+  // match a cleaned up message
+
+  const newsChannel = "<#428888110634500097>";
+  const faqChannel =  "<#427877188990402571>";
+
+  let dictionary = {
+
+    'stats':        {response: `!price, !volume, !marketcap, !supply, !hashrate, !difficulty, !blocktime, !retarget :chart_with_upwards_trend:`},
+    'blockchain':   {response: `${data.hashrate.toPrecision(2)} GH, ${Math.round(data.difficulty)} diff, ${data.blockTime.toFixed(1)} min/block, ${data.retarget} blocks and ${(data.retarget * data.blockTime / 60).toPrecision(2)} hours to retarget`},
+    'market':       {response: `we're trading at ${data.price * 1e8} satoshi, with ${data.volume.toPrecision(2)} BTC daily volume, and a ${data.marketCap.toPrecision(2)} BTC market cap`},
+
+    'price':        {response: `${data.price * 1e8} satoshi`},
+    'volume':       {response: `${data.volume.toPrecision(2)} BTC per day`},
+    'marketcap':    {response: `${data.marketCap.toPrecision(2)} BTC`},
+    'supply':       {response: `${Number((data.supply / 1e6).toPrecision(2))}M PGN`},
+    'hashrate':     {response: `${data.hashrate.toPrecision(2)} GH`},
+    'difficulty':   {response: `${Math.round(data.difficulty)} for the next ${(data.retarget * data.blockTime / 60).toPrecision(2)} hours`},
+    'blocktime':    {response: `${data.blockTime.toFixed(1)} minutes`},
+    'retarget':     {response: `${data.retarget} blocks, ${(data.retarget * data.blockTime / 60).toPrecision(2)} hours`},
+
+
+    'pool':         {response: `https://pool.pigeoncoin.org/ *Supports development*\nOther pools can be found in ${faqChannel}`},
+    'explorer':     {response: `https://explorer.pigeoncoin.org`},
+    'website':      {response: `https://pigeoncoin.org`},
+    'exchange':     {response: `we have trading pairs with Ravencoin, Litecoin, and Bitcoin on CryptoBridge! https://crypto-bridge.org/`},
+    'roadmap':      {response: `roadmap is in progress! We will announce updates in ${newsChannel}`},
+    'whitepaper':   {response: `the X16S (shuffle) mini-whitepaper is here https://pigeoncoin.org/assets/X16S-whitepaper.pdf`},
+    'whattomine':   {response: `Pigeoncoin, of course! https://pool.pigeoncoin.org/`},
+    'miners':       {response: `https://pigeoncoin.org/mining`},
+    'release':      {response: `https://github.com/Pigeoncoin/pigeoncoin/releases`},
+    'donate':       {response: 'please donate!\n\nPigeoncoin: `PDiZ89gk5HMqwrcGp6Hs9WdgFALzLbD4HG` \nBitcoin: `1NaVP4cKiWY6MxSDkTCZ2kh5Xm3coA27Qv`'},
+
+    'mobile':       {response: `keep an eye out for the roadmap!  ${newsChannel}`},
+
+    'masternode':   {response: `never.`},
+
+    'coinmarketcap':       {response: `when we have $100k USD daily volume on all exchanges.`},
+    'coingecko':       {response: `we're there! https://www.coingecko.com/en/coins/pigeoncoin`},
+    'livecoinwatch':       {response: `livecoinwatch listing is in progress!`},
+    'whattomine':       {response: `we need to be listed on Abucoins, Bitfinex, Bittrex, Binance, Cryptopia, HitBTC, Poloniex or YoBit first!`},
+
+
+    'cryptobridge': {response: `we have trading pairs with Ravencoin, Litecoin, and Bitcoin on CryptoBridge! https://crypto-bridge.org/`},
+    'cobinhood': {response: `we are speaking with Cobinhood and are expecting a determination around Q3 2018.`},
+    'cryptopia': {response: 'when you donate all your Pigeon to `PDiZ89gk5HMqwrcGp6Hs9WdgFALzLbD4HG`'},
+    'bittrex': {response: 'when you donate all your Pigeon to `PDiZ89gk5HMqwrcGp6Hs9WdgFALzLbD4HG`'},
+    'binance': {response: 'when you donate all your Pigeon to `PDiZ89gk5HMqwrcGp6Hs9WdgFALzLbD4HG`'},
+    'coinbase': {response: 'when you donate all your Pigeon and your first born child to `PDiZ89gk5HMqwrcGp6Hs9WdgFALzLbD4HG`'},
+
+    'moon': {response: `fly pigeon, fly! :rocket:`},
+    'lambo': {response: `when lambo?! When land!`},
+    'surfin': {response: `:surfer:\nhttps://www.youtube.com/watch?time_continue=80&v=gBexh377HbQ`},
+    'handsome boy': {files: ['./img/handsome-boy.jpg']},
+    'nasdaq': {response: 'why not?'},
+    'good bot': {files: ['./img/good-bot.jpg']},
+
+    'stuck transaction': {response: `check this out https://bitzuma.com/posts/how-to-clear-a-stuck-bitcoin-transaction/`}
+
+  }
+
+  const shorthand = [
+    {key: 'diff', inherits: 'difficulty'},
+    {key: 'diff change', inherits: 'retarget'},
+    {key: 'difficulty change', inherits: 'retarget'},
+
+    {key: 'wallet', inherits: 'release'},
+
+    {key: 'ios', inherits: 'mobile'},
+    {key: 'android', inherits: 'mobile'},
+    {key: 'spv', inherits: 'mobile'},
+    {key: 'mobile wallet', inherits: 'mobile'},
+    {key: 'spv wallet', inherits: 'mobile'},
+
+    {key: 'airdrop', inherits: 'masternode'},
+    {key: 'faucet', inherits: 'masternode'},
+
+    {key: 'cmc', inherits: 'coinmarketcap'},
+
+    {key: 'cb', inherits: 'cryptobridge'},
+    {key: 'gdax', inherits: 'coinbase'}
+  ]
+
+
+  for(item in shorthand){
+    const newKey = shorthand[item].key
+    const inherits = shorthand[item].inherits
+
+    if(dictionary[inherits]){
+        dictionary[newKey] = dictionary[inherits]
+    }
+  }
+
+
+  const matches = stringSimilarity.findBestMatch(cleanedMessage, Object.keys(dictionary));
+
+  if(matches.bestMatch.rating > 0.6){
+    return Object.assign(dictionary[matches.bestMatch.target],{'rating':matches.bestMatch.rating})
+  }
+}
+
+
+//msg.reply(`!price, !volume, !marketcap, !supply, !hashrate, !difficulty, !blocktime, !retarget :chart_with_upwards_trend:`);
 
 
 
@@ -71,232 +197,21 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  var message = msg.content.toLowerCase();
+client.on('message', message => {
+  if( message.content.trim().startsWith('!') ){
+    let replyObject = getReply(cleanMessage(message.content))
 
+    if(replyObject){
 
-  //////////////////////
-  // channels
-  //////////////////////
-  var newsChannel = "<#428888110634500097>";
-  var faqChannel =  "<#427877188990402571>";
+      message.reply(replyObject.response, {
+        files: replyObject.files
+      });
+    }
 
-
-
-  //////////////////////
-  // stats
-  //////////////////////
-
-
-  if (message.startsWith("!stats")){
-    msg.reply(`!price, !volume, !marketcap, !supply, !hashrate, !difficulty, !blocktime, !retarget :chart_with_upwards_trend:`);
   }
-
-
-  if (message.startsWith("!price")){
-    msg.reply(`${data.price * 1e8} satoshi`);
-  }
-
-  if (message.startsWith("!volume")){
-    msg.reply(`${data.volume.toPrecision(2)} BTC per day`);
-  }
-
-  if (message.startsWith("!marketcap")){
-    msg.reply(`${data.marketCap.toPrecision(2)} BTC`);
-  }
-
-  if (message.startsWith("!supply")){
-    msg.reply(`${Number((data.supply / 1e6).toPrecision(2))}M PGN`);
-  }
-
-  if (message.startsWith("!hash")){
-    msg.reply(`${data.hashrate.toPrecision(2)} GH`);
-  }
-
-  if (message.startsWith("!diff")){
-    msg.reply(`${Math.round(data.difficulty)}`);
-  }
-
-  if (message.startsWith("!blocktime")){
-    msg.reply(`${data.blockTime.toPrecision(2)} minutes`);
-  }
-
-  if (message.startsWith("!retarget")){
-    msg.reply(`${data.retarget} blocks, ${(data.retarget * data.blockTime / 60).toPrecision(2)} hours`);
-  }
-
-
-
-  //////////////////////
-  // general
-  //////////////////////
-
-  if (message.startsWith("!pool") ||
-      message.startsWith("!when pool")){
-    msg.reply(`
-      https://pool.pigeoncoin.org/ *Supports development*
-      Other pools can be found in ${faqChannel}`);
-  }
-
-  if (message.startsWith("!when explorer") ||
-      message.startsWith("!explorer")){
-    msg.reply('https://explorer.pigeoncoin.org');
-  }
-
-  if (message.startsWith("!when website") ||
-      message.startsWith("!website")){
-    msg.reply('https://pigeoncoin.org');
-  }
-
-  if (message.startsWith("!when exchange") ||
-      message.startsWith("!exchange")){
-    msg.reply(`we have trading pairs with Ravencoin, Litecoin, and Bitcoin on CryptoBridge! https://crypto-bridge.org/`);
-  }
-
-  if (message.startsWith("!when roadmap") ||
-      message.startsWith("!roadmap")){
-    msg.reply(`roadmap is in progress! We will announce updates in ${newsChannel}`);
-  }
-
-  if (message.startsWith("!when whitepaper") ||
-      message.startsWith("!whitepaper")){
-    msg.reply(`the X16S (shuffle) mini-whitepaper is here https://pigeoncoin.org/assets/X16S-whitepaper.pdf`);
-  }
-
-  if (message.startsWith("!whattomine")){
-    msg.reply(`Pigeoncoin, of course! https://pool.pigeoncoin.org/`);
-  }
-
-  if (message.startsWith("!mining") ||
-      message.startsWith("!miner")){
-    msg.reply(`https://pigeoncoin.org/mining`);
-  }
-
-
-  if (message.startsWith("!release") ||
-      message.startsWith("!wallet")){
-    msg.reply(`https://github.com/Pigeoncoin/pigeoncoin/releases`);
-  }
-
-  if (message.startsWith("!donate") ||
-      message.startsWith("!when donate")){
-    msg.reply('please donate!\n\nPigeoncoin: `PDiZ89gk5HMqwrcGp6Hs9WdgFALzLbD4HG` \nBitcoin: `1NaVP4cKiWY6MxSDkTCZ2kh5Xm3coA27Qv`');
-  }
-
-  if (message.startsWith("!when ios") ||
-      message.startsWith("!when android")){
-    msg.reply('keep an eye out for the roadmap! ' + newsChannel);
-  }
-
-
-
-  //////////////////////
-  // features that wont happen
-  //////////////////////
-
-  if (message.startsWith("!when masternode") ||
-      message.startsWith("!when airdrop") ||
-      message.startsWith("!when faucet")){
-    msg.reply('never.');
-  }
-
-
-  //////////////////////
-  // help 
-  //////////////////////
-
-  if (message.startsWith("!help stuck transaction")){
-    msg.reply('check this out https://bitzuma.com/posts/how-to-clear-a-stuck-bitcoin-transaction/');
-  }
-
-
-  //////////////////////
-  // listings
-  //////////////////////
-
-  if (message.startsWith("!when cmc") ||
-      message.startsWith("!when coinmarketcap") ||
-      message.startsWith("!when coin market cap") ){
-    msg.reply('when we have $100k USD daily volume on all exchanges.');
-  }
-
-  if (message.startsWith("!when coingecko") ||
-      message.startsWith("!when cg") ||
-      message.startsWith("!when coin gecko") ){
-    msg.reply('we updated Explorer API to meet their needs and have submitted a request for listing!');
-  }
-
-  if (message.startsWith("!when lcw") ||
-      message.startsWith("!when livecoinwatch") ||
-      message.startsWith("!when live coin watch") ){
-    msg.reply('livecoinwatch listing is in progress!');
-  }
-
-  if (message.startsWith("!when wtm") ||
-      message.startsWith("!when whattomine") ||
-      message.startsWith("!when what to mine") ){
-    msg.reply('we need to be listed on Abucoins, Bitfinex, Bittrex, Binance, Cryptopia, HitBTC, Poloniex or YoBit first!');
-  } // see "!whattomine" in //general
-
-
-  //////////////////////
-  // exchanges
-  //////////////////////
-
-  if (message.startsWith("!when cb") ||
-      message.startsWith("!when cryptobridge") ||
-      message.startsWith("!when crypto bridge")){
-    msg.reply('we have trading pairs with Ravencoin, Litecoin, and Bitcoin on CryptoBridge! https://crypto-bridge.org/');
-  }
-
-  if (message.startsWith("!when cobinhood") ||
-      message.startsWith("!when cob")){
-    msg.reply('cobinhood listing is in progress.');
-  }
-
-  if (message.startsWith("!when cryptopia")){
-    msg.reply('when you donate all your Pigeon to `PDiZ89gk5HMqwrcGp6Hs9WdgFALzLbD4HG`');
-  }
-
-  if (message.startsWith("!when bittrex")){
-    msg.reply('when you donate all your Pigeon to `PDiZ89gk5HMqwrcGp6Hs9WdgFALzLbD4HG`');
-  }
-
-  if (message.startsWith("!when binance")){
-    msg.reply('when you donate all your Pigeon to `PDiZ89gk5HMqwrcGp6Hs9WdgFALzLbD4HG`');
-  }
-
-  if (message.startsWith("!when coinbase") ||
-      message.startsWith("!when gdax")){
-    msg.reply('when you donate all your Pigeon and your first born child to `PDiZ89gk5HMqwrcGp6Hs9WdgFALzLbD4HG`');
-  }
-
-
-  //////////////////////
-  // fun
-  //////////////////////
-
-  if (message.startsWith("!when moon")){
-    msg.reply('fly pigeon, fly! :rocket:');
-  }
-
-  if (message.startsWith("!when lambo")){
-    msg.reply('when lambo?! When land!');
-  }
-
-  if (message.startsWith("!surfin")){
-    msg.reply(':surfer:\nhttps://www.youtube.com/watch?time_continue=80&v=gBexh377HbQ');
-  }
-
-  if (message.startsWith("!handsom")){
-    msg.reply('', {
-      files:['./img/handsome-boy.jpg']
-    });
-  }
-
-
-
-
 });
+
+
+
 
 client.login(settings.key);
