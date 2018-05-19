d658a75cf1da01d99c5e8d730258ce52c902364f
Luke Pighetti <lukepighetti@gmail.com>
2018-05-19 14:13:42 +0000

    refactored getReply

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const getReply = require('./lib/strings.js')
+const getReply = require('./lib/getReply.js')
+const sendReply = require('./lib/sendReply.js')
 const settings = require('./settings');
+
 
 // setup discord
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 client.login(settings.key);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
 
 // setup firebase
 const firebase = require('firebase')
@@ -29,69 +33,10 @@ latestRef.on('value', snap => {
 })
 
 // listen for new messages
-
 client.on('message', message => {
   if( message.content.trim().startsWith('!') ){
-    let replyObject = getReply(message.content, latestData)
+    const reply = getReply(message.content, latestData)
 
-    // reply is allowed if the role is hoisted from @everyone
-    // and the message is not in general
-    // reactions are allowed anywhere
-    const spamChannel = 439057355968217088
-    let hoisted = null
-    let privateMessage = null
-
-
-    if(message.member){
-        hoisted = !!message.member.roles.find("hoist", true)
-    }else{
-        privateMessage = true
-    }
-
-    const isAllowed = message.channel.id == spamChannel || hoisted || privateMessage
-
-    if(replyObject){
-      let reaction = replyObject.reaction
-      let response = replyObject.response
-      let images = replyObject.files
-
-      // reactions
-      if(reaction) {
-        if(typeof reaction === 'string') reaction = [reaction]; // convert strings to arrays
-
-        for(emoji in reaction){
-          if(reaction[emoji].length > 10){
-            message.react(message.guild.emojis.get(reaction[emoji]))
-          }
-          else {
-            message.react(reaction[emoji])
-          }
-        }
-      }
-
-      // response
-      if((response || images)){
-          // are they allowed to bot in this channe?
-          if(isAllowed){
-              // reply in channel
-              message.reply(response, {
-                files: images
-              });
-          }else{
-              // delete message
-              // PM sender
-              message.react('👋')
-                .then(message.delete(2000))
-
-              message.author.send(response, {
-                files: images
-              })
-          }
-      }
-    }else{
-        message.react('❓')
-          .then(message.delete(2000))
-    }
-
+    sendReply(message, reply)
   }
 });
