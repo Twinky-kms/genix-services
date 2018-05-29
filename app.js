198e393be2d61e43930b9b92f63578792c3f112a
Luke Pighetti <lukepighetti@gmail.com>
2018-05-29 15:28:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// get data from Firebase
+const firebase = require('firebase')
+
+console.log(firebase)
+//  latestData
+//  historyData
+
+// serve it via Express
+//  legacy format /v1/
+//  latestData /v2/latestData/
+//  historyData /v2/historyData/
