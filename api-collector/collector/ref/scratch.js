b0216e101b2d3c1a8b7c2912ac82da5e2516a9f3
Luke Pighetti <lukepighetti@gmail.com>
2019-01-18 12:08:39 -0500

    delete dead code

--- api-collector/collector/ref/scratch.js
+++
@@ -1,8 +0,0 @@
-const { getMiningInfo, getLastBlockHeight } = require("./lib/rpc");
-
-main();
-
-async function main() {
-  console.log(await getMiningInfo());
-  console.log(await getLastBlockHeight());
-}
